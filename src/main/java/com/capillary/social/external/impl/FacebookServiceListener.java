a6f35a1f198b9c2c7299e9570376d233eae6d7e8
Abhinav <abhinav@CAP-LAP-643>
2016-12-15 01:39:15 +0530

    remove commented lines

--- src/main/java/com/capillary/social/external/impl/FacebookServiceListener.java
+++ src/main/java/com/capillary/social/external/impl/FacebookServiceListener.java
@@ -23,17 +23,15 @@ public class FacebookServiceListener implements Iface {
 
 	@Override
 	public boolean isAlive() throws TException {
-		// TODO Auto-generated method stub
+
 		logger.info("Is alive called");
 		return true;
 	}
 
-	// TODO: remove accessToken on the method contract
-	// Receive senderId=pageId and retrieve accessToken
 	@Override
 	public boolean sendMessage(String recipientId, String messageText,
 			String pageId, int orgId) throws FacebookException, TException {
-		// TODO Auto-generated method stub
+
 		logger.info("send message called: Recipient Id: " + recipientId
 				+ "Message Text: " + messageText + "Page Id: " + pageId
 				+ "Org Id: " + orgId);
